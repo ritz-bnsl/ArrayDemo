c61de6c8b14034b68ea51163111519db4eeec994
ritz-bnsl <ritika.bansal_cs18@gla.ac.in>
2020-01-08 21:05:22 +0530

    init commit;

--- src/array/ArrayDemo.java
+++ src/array/ArrayDemo.java
@@ -7,6 +7,49 @@
 
 package array;
 
-public class ArrayDemo {
+import java.util.Arrays;
 
+public class ArrayDemo {
+    public static void main(String[] agrs) {
+        int[] integers = new int[10];
+        printArrayValues(integers);
+        initialIntegerArray(integers);
+        printArrayValues(integers);
+        boolean response = searchInArray(integers, 22);
+        if (response) {
+            System.out.println("Value found");
+        } else {
+            System.out.println("Value not found");
+        }
+        System.out.println(response ? "value found" : "value not found");
+    }
+
+    /**
+     * this method can search a integer value in integer array
+     *
+     * @param array an integer array
+     * @param value the value to be searched
+     * @return True if the (@param value) is found in the (@param array)
+     */
+    public static boolean searchInArray(int[] array, int value) {
+        boolean response = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                response = true;
+                break;
+            }
+        }
+        return response;
+    }
+
+    public static void printArrayValues(int[] integers) {
+        System.out.println(Arrays.toString(integers));
+    }
+
+    public static void initialIntegerArray(int[] integerArray) {
+        for (int i = 0; i < integerArray.length; i++) {
+            integerArray[i] = (i + 1);
+        }
+    }
 }
+
